8b1bec15344df49fc3cc691cb7585a588241589a
youhobin <ghdb132@naver.com>
2023-05-09 21:03:23 +0900

    ProductNumber Test

---
+++ src/main/java/sample/cafekiosk/spring/api/controller/product/dto/request/ProductCreateRequest.java
@@ -0,0 +1,17 @@
+package sample.cafekiosk.spring.api.controller.product.dto.request;
+
+import lombok.Getter;
+import sample.cafekiosk.spring.domain.product.ProductSellingStatus;
+import sample.cafekiosk.spring.domain.product.ProductType;
+
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+
+@Getter
+public class ProductCreateRequest {
+
+    private ProductType type;
+    private ProductSellingStatus sellingStatus;
+    private String name;
+    private int price;
+}
--- src/main/java/sample/cafekiosk/spring/api/service/product/ProductService.java
+++ src/main/java/sample/cafekiosk/spring/api/service/product/ProductService.java
@@ -2,6 +2,7 @@ package sample.cafekiosk.spring.api.service.product;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import sample.cafekiosk.spring.api.controller.product.dto.request.ProductCreateRequest;
 import sample.cafekiosk.spring.api.service.product.response.ProductResponse;
 import sample.cafekiosk.spring.domain.product.Product;
 import sample.cafekiosk.spring.domain.product.ProductRepository;
@@ -16,6 +17,13 @@ public class ProductService {
 
     private final ProductRepository productRepository;
 
+    public void createProduct(ProductCreateRequest request) {
+        // productNumber 001 002 등
+        // DB에서 마지막 저장된 Product의 상품 번호를 읽어와서 +1
+        String latestProductNumber = productRepository.findLatestProductNumber();
+
+    }
+
     public List<ProductResponse> getSellingProducts() {
         List<Product> products = productRepository.findAllBySellingStatusIn(ProductSellingStatus.forDisplay());
 
@@ -23,5 +31,4 @@ public class ProductService {
                 .map(ProductResponse::of)
                 .collect(Collectors.toList());
     }
-
 }
