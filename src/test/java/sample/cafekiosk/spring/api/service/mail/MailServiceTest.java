b58bde10db5769cdcb002aa2ef7003b5aa7ece9e
youhobin <ghdb132@naver.com>
2023-05-11 11:53:30 +0900

    순수한 mockito 테스트

---
+++ src/test/java/sample/cafekiosk/spring/api/service/mail/MailServiceTest.java
@@ -0,0 +1,54 @@
+package sample.cafekiosk.spring.api.service.mail;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import sample.cafekiosk.spring.client.mail.MailSendClient;
+import sample.cafekiosk.spring.domain.history.mail.MailSendHistory;
+import sample.cafekiosk.spring.domain.history.mail.MailSendHistoryRepository;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+
+// 순수하게 mockito 로만 테스트 쓸 수 있도록. mockbean 이 아닌
+@ExtendWith(MockitoExtension.class)
+class MailServiceTest {
+
+    @Mock
+    private MailSendClient mailSendClient;
+
+    @Mock
+    private MailSendHistoryRepository mailSendHistoryRepository;
+
+    @InjectMocks
+    private MailService mailService;
+
+    @DisplayName("메일 전송 테스트")
+    @Test
+    void sendMail() {
+        // given
+        when(mailSendClient.sendEmail(anyString(), any(String.class), any(String.class), any(String.class)))
+                .thenReturn(true);
+
+        // Spy 를 사용하면 진짜 기능 객체도 사용하고 일부만 stubbing 하고 싶을때 사용.
+//        doReturn(true)
+//                .when(mailSendClient)
+//                .sendEmail(anyString(), any(String.class), any(String.class), any(String.class));
+
+        // when
+        boolean result = mailService.sendMail("", "", "", "");
+
+        //then
+        Assertions.assertThat(result).isTrue();
+        verify(mailSendHistoryRepository, times(1)).save(any(MailSendHistory.class));
+
+    }
+
+}
